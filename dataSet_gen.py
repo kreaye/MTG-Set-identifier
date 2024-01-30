fe9741d678a509f0f452d973553dc94be2ec82cd
ancientdragon12 <kxr8350@rit.edu>
2024-01-30 11:13:37 -0500

    inital main setup
    Added json of card printing types to know which to use and what not to use. set up initial main function

---
+++ dataSet_gen.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+def main():
+    None
+#import list of layout types
+
+#downlooad current list of cards from scryfall
+
+#sort list of cards downloading images from layouts that are acceptable
+
+#crop out the set images
+
+#aply any filters and variations to the cards
+
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        print("process failed")
